c14881303e1423ff37e4da8d2b28e7297c31625d
OhmGeek <ryan@ohmgeek.co.uk>
2017-02-13 23:47:01 +0000

    Order server written

---
+++ order_server.py
@@ -0,0 +1,41 @@
+import Pyro4
+
+
+@Pyro4.expose
+class OrderManager(object):
+    """ This is a manager to deal with orders via Pyro """
+    def __init__(self):
+        # create a list of orders
+        self.orders = {}
+
+    def __create_user(self, userid):
+        self.orders[userid] = []
+
+    def __format_order_hist(self, userid):
+        output = ""
+        history = self.orders[userid]
+        index = -1
+        for order in history:
+            index += 1
+            output += "ID: " + str(index) + "      items: " + str(order)
+            output += "\n\n"
+        return output
+
+    def place_order(self, userid, item_list):
+        """ Place an order """
+        if len(item_list) > 3 or len(item_list) <= 0:
+            print("Error, most contain at most 3 items")
+        if userid not in self.orders:
+            self.__create_user(userid)
+
+        self.orders[userid].append(item_list)
+        # get the index of the last element in the list
+        return len(self.orders[userid]) - 1
+
+    def get_order_history(self, userid):
+        """ Get a user's order history """
+        if userid not in self.orders:
+            print("User not found")
+        # otherwise, format the order history and return it.
+        formatted_history = self.__format_order_hist(userid)
+        return formatted_history
